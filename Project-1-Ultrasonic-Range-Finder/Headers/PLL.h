4204b529cd0dcab40083779efcbba6ea39a99fd0
Eric Santana <44712870+Eric2363@users.noreply.github.com>
2025-12-14 15:59:40 -0800

    Delete Project-1-Ultrasonic-Range-Finder directory

--- Project-1-Ultrasonic-Range-Finder/Headers/PLL.h
+++
@@ -1,5 +0,0 @@
-#ifndef PLL_H
-#define PLL_H
-#define SYSDIV2 24
-void PLL_Init(void);
-#endif
--- Project-1-Ultrasonic-Range-Finder/Src/SysTick.c
+++
@@ -1,8 +0,0 @@
-#include "tm4c123gh6pm.h"
-#include "SysTick.h"
-void SysTick_Init(void){
-  NVIC_ST_CTRL_R = CLEAR_REG;
-  NVIC_ST_RELOAD_R = HALF_SEC - 1;
-  NVIC_ST_CURRENT_R = CLEAR_REG;
-  NVIC_ST_CTRL_R = EN_SYSTICK;
-}
